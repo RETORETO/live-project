08da37e4f4424aa86b1927ef91d8084d56042a3b
Reto <59349681+RETORETO@users.noreply.github.com>
2020-03-15 13:59:54 +0800

    Create Order.java

---
+++ src/pojo/Order.java
@@ -0,0 +1,31 @@
+package pojo;
+
+public class Order {
+    private int orderNum;
+    private int prize;
+    private int orderCount;
+    
+    public int getOrderNum() {
+    	return orderNum;
+    }
+    
+    public void setOrderNum(int orderNum) {
+    	this.orderNum = orderNum;
+    }
+    
+    public int getPrize() {
+    	return prize;
+    }
+    
+    public void setPrize(int prize) {
+    	this.prize = prize;
+    }
+    
+    public int getOrderCount() {
+    	return orderCount;
+    }
+    
+    public void setOrderCount(int orderCount) {
+    	this.orderCount = orderCount;
+    }
+}
