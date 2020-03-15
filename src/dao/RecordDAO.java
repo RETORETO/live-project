bddfceaa80808760e292632ec87a6ee47f023076
Reto <59349681+RETORETO@users.noreply.github.com>
2020-03-15 18:57:11 +0800

    Update RecordDAO.java

--- src/dao/RecordDAO.java
+++ src/dao/RecordDAO.java
@@ -1,8 +1,12 @@
 package dao;
 
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import pojo.Record;
 
 public interface RecordDAO {
 	
 	void add(Record record);
+	public void findInfo(JPanel panel,JTextField text);
 }
